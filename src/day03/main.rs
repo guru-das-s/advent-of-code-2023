8d5fcb87e18e6c0c14e3a05796704ccf33653bfc
Guru Das Srinagesh <gurooodas@gmail.com>
2023-12-03 14:33:35 -0800

    day03: Part 1: off by one on columns
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- src/day03/main.rs
+++ src/day03/main.rs
@@ -1,5 +1,34 @@
 const TEST_INPUT: &str = include_str!("testinput");
 
+fn check_if_part_num(
+    schematic: &Vec<Vec<char>>,
+    row: usize,
+    col_start: usize,
+    col_end: usize,
+) -> bool {
+    let height = schematic.len();
+    let width = schematic[0].len();
+
+    let row_up = if row > 0 { row - 1 } else { 0 };
+    let row_down = (row + 1).min(height);
+    let col_left = if col_start > 0 { col_start - 1 } else { 0 };
+    let col_right = (col_end + 1).min(width);
+
+    print!("({} {})({} {}): ", row_up, col_left, row_down, col_right);
+
+    for i in row_up..row_down {
+        for j in col_left..col_right {
+            let c = schematic[i][j];
+            if !c.is_ascii_digit() && c != '.' {
+                // Yay! It's a part number.
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
 fn main() {
     let mut schematic: Vec<Vec<char>> = Vec::new();
 
@@ -9,8 +38,6 @@ fn main() {
 
     let height = schematic.len();
     let width = schematic[0].len();
-
-    println!("Height: {}, width: {}", height, width);
 
     let mut row: usize = 0;
     let mut col: usize = 0;
@@ -37,7 +64,9 @@ fn main() {
                 .collect::<String>()
                 .parse::<u32>()
                 .unwrap();
-            print!("{} ", num);
+            print!("{}: ", num);
+            let is_part_num = check_if_part_num(&schematic, row, num_start, num_end);
+            print!("{}, ", is_part_num);
             col = col + 1;
         }
         row = row + 1;
