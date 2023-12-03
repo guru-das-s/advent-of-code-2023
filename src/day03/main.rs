74183f2b3c0d72b131b4936e3d34d43ae1eddd8f
Guru Das Srinagesh <gurooodas@gmail.com>
2023-12-03 12:58:02 -0800

    day03: Try a different approach
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- src/day03/main.rs
+++ src/day03/main.rs
@@ -1,14 +1,10 @@
 const TEST_INPUT: &str = include_str!("testinput");
 
-fn find_first_ascii_digit_index(data: &[u8]) -> Option<usize> {
-    data.iter().position(|&byte| byte >= b'0' && byte <= b'9')
-}
-
 fn main() {
-    let mut schematic: Vec<Vec<u8>> = Vec::new();
+    let mut schematic: Vec<Vec<char>> = Vec::new();
 
     for line in TEST_INPUT.lines() {
-        schematic.push(line.as_bytes().to_vec());
+        schematic.push(line.chars().collect::<Vec<_>>());
     }
 
     let height = schematic.len();
@@ -16,13 +12,16 @@ fn main() {
 
     println!("Height: {}, width: {}", height, width);
 
-    for (i, row) in schematic.iter().enumerate() {
-        print!("Row {}: ", i);
-        let mut start_index = 0;
-        while let Some(next_index) = find_first_ascii_digit_index(&row[start_index..]) {
-            print!("{} ", start_index + next_index);
-            start_index += next_index + 1;
+    let mut row: usize = 0;
+    let mut col: usize = 0;
+
+    while row < height {
+        while col < width {
+            print!("{}", schematic[row][col]);
+            col = col + 1;
         }
+        row = row + 1;
+        col = 0;
         println!("");
     }
 }
