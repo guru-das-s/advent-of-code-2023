6f8a4ee67f8cc1bec337bc74abd5c68bbc4d9c0c
Guru Das Srinagesh <gurooodas@gmail.com>
2023-12-05 22:21:06 -0800

    day05: Solve for Part 1 answer YAY!
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- src/day05/main.rs
+++ src/day05/main.rs
@@ -1,10 +1,11 @@
 use regex::Regex;
 
 const TEST_INPUT: &str = include_str!("testinput");
+const INPUT: &str = include_str!("input");
 
-fn get_numvec_from_string(input: &str) -> Vec<u32> {
+fn get_numvec_from_string(input: &str) -> Vec<u64> {
     let nums_re = Regex::new(r"([0-9\s]+)+").unwrap();
-    let mut numvec: Vec<u32> = Vec::new();
+    let mut numvec: Vec<u64> = Vec::new();
     for cap in nums_re.captures_iter(input) {
         numvec = cap
             .get(1)
@@ -21,10 +22,10 @@ fn line_has_only_nums(line: &str) -> bool {
     let nums_re = Regex::new(r"([0-9\s]+)+").unwrap();
     nums_re.is_match(line)
 }
-fn parse_input(input: &str) -> (Vec<u32>, Vec<Vec<Vec<u32>>>) {
+fn parse_input(input: &str) -> (Vec<u64>, Vec<Vec<Vec<u64>>>) {
     let re = Regex::new(r"seeds:\s([0-9\s]+)+").unwrap();
 
-    let mut seeds: Vec<u32> = Vec::new();
+    let mut seeds: Vec<u64> = Vec::new();
 
     for cap in re.captures_iter(input) {
         seeds = cap
@@ -35,7 +36,7 @@ fn parse_input(input: &str) -> (Vec<u32>, Vec<Vec<Vec<u32>>>) {
             .map(|num| num.parse().unwrap())
             .collect();
     }
-    println!("{:?}", seeds);
+    // println!("{:?}", seeds);
 
     // Store line numbers of the text markers
     let mut text_marker_indices: Vec<usize> = Vec::new();
@@ -52,23 +53,23 @@ fn parse_input(input: &str) -> (Vec<u32>, Vec<Vec<Vec<u32>>>) {
             _ => continue,
         }
     }
-    println!("{:?}", text_marker_indices);
+    // println!("{:?}", text_marker_indices);
 
-    let mut all_map_numbers: Vec<Vec<Vec<u32>>> = Vec::new();
+    let mut all_map_numbers: Vec<Vec<Vec<u64>>> = Vec::new();
     for index in text_marker_indices.iter() {
         let line_iter = input.lines().skip(*index + 1);
-        let x2x_lines: Vec<Vec<u32>> = line_iter
+        let x2x_lines: Vec<Vec<u64>> = line_iter
             .take_while(|line| line_has_only_nums(line))
             .map(|line| get_numvec_from_string(line))
             .collect();
         all_map_numbers.push(x2x_lines);
     }
 
-    println!("{:?}", all_map_numbers);
+    // println!("{:?}", all_map_numbers);
     (seeds, all_map_numbers)
 }
 
-fn walk_map(num: u32, map: &Vec<Vec<u32>>) -> u32 {
+fn walk_map(num: u64, map: &Vec<Vec<u64>>) -> u64 {
     for entry in map.iter() {
         let range = entry[1]..(entry[1] + entry[2] + 1);
         if range.contains(&num) {
@@ -78,8 +79,8 @@ fn walk_map(num: u32, map: &Vec<Vec<u32>>) -> u32 {
     num
 }
 
-fn lookup_seed_location(seed: u32, all_map_numbers: &Vec<Vec<Vec<u32>>>) -> u32 {
-    let mut location: u32 = seed;
+fn lookup_seed_location(seed: u64, all_map_numbers: &Vec<Vec<Vec<u64>>>) -> u64 {
+    let mut location: u64 = seed;
     for map in all_map_numbers.iter() {
         location = walk_map(location, map)
     }
@@ -87,15 +88,17 @@ fn lookup_seed_location(seed: u32, all_map_numbers: &Vec<Vec<Vec<u32>>>) -> u32 
 }
 
 fn main() {
-    let (seeds, all_map_numbers) = parse_input(TEST_INPUT);
-    assert_eq!(walk_map(98, &all_map_numbers[0]), 50);
-    assert_eq!(walk_map(53, &all_map_numbers[0]), 55);
-    assert_eq!(walk_map(10, &all_map_numbers[0]), 10);
+    let (seeds, all_test_map_numbers) = parse_input(TEST_INPUT);
+    assert_eq!(walk_map(98, &all_test_map_numbers[0]), 50);
+    assert_eq!(walk_map(53, &all_test_map_numbers[0]), 55);
+    assert_eq!(walk_map(10, &all_test_map_numbers[0]), 10);
 
-    assert_eq!(lookup_seed_location(79, &all_map_numbers), 82);
-    assert_eq!(lookup_seed_location(13, &all_map_numbers), 35);
+    assert_eq!(lookup_seed_location(79, &all_test_map_numbers), 82);
+    assert_eq!(lookup_seed_location(13, &all_test_map_numbers), 35);
 
-    let mut all_locations: Vec<u32> = Vec::new();
+    let (seeds, all_map_numbers) = parse_input(INPUT);
+
+    let mut all_locations: Vec<u64> = Vec::new();
     for seed in seeds.iter() {
         all_locations.push(lookup_seed_location(*seed, &all_map_numbers));
     }
