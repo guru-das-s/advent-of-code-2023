3269f9ba98c68dfeefdf2fc75bbd02cd87baa31b
Guru Das Srinagesh <gurooodas@gmail.com>
2024-01-14 11:22:55 -0800

    day06: parse INPUT correctly
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- src/day06/main.rs
+++ src/day06/main.rs
@@ -1,3 +1,5 @@
+const INPUT: &str = include_str!("input");
+
 fn ways_to_win(max_t: i32, max_d: i32) -> i32 {
     // t * (max_t - t) = d
     // -t^2 + max_t * t - d = 0
@@ -13,8 +15,24 @@ fn ways_to_win(max_t: i32, max_d: i32) -> i32 {
     (t2.ceil() - t1.floor() - 1 as f64) as i32
 }
 
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
+    let mut times_and_distances: Vec<Vec<i32>> = Vec::new();
+    for line in input.lines() {
+        let mut nums: Vec<i32> = Vec::new();
+        nums = line
+            .trim()
+            .split_whitespace()
+            .flat_map(str::parse::<i32>)
+            .collect::<Vec<i32>>();
+        times_and_distances.push(nums);
+    }
+    times_and_distances
+}
+
 fn main() {
     assert_eq!(ways_to_win(7, 9), 4);
     assert_eq!(ways_to_win(15, 40), 8);
     assert_eq!(ways_to_win(30, 200), 9);
+
+    println!("{:?}", parse_input(INPUT));
 }
